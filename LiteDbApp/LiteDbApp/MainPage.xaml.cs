f322b44327f9a788335747cd9f34880cdb850b2e
johnny <johnny@wantgoo.com>
2018-02-21 17:12:02 +0800

    initial commit

---
+++ LiteDbApp/LiteDbApp/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using LiteDB;
+using LiteDbApp.ViewModels;
+using Xamarin.Forms;
+
+namespace LiteDbApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            var litedbConnectionString =
+                $"Filename={Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "litedb.db")}";
+
+            using (var db = new LiteDatabase(litedbConnectionString))
+            {
+                var tests = db.GetCollection("test");
+
+                tests.Upsert(new BsonDocument { ["_id"] = 1, ["Name"] = $"Hello World {DateTime.Now:yyyy-MM-dd HH:mm:ss}" });
+            }
+
+            using (var db = new LiteDatabase(litedbConnectionString))
+            {
+                var tests = db.GetCollection("test");
+
+                this.MainPageViewModel.Message = tests.FindById(1)["Name"];
+            }
+        }
+
+        private MainPageViewModel MainPageViewModel => (MainPageViewModel)this.BindingContext;
+    }
+}
---
+++ LiteDbApp/LiteDbApp/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace LiteDbApp.ViewModels
+{
+    public class MainPageViewModel : BindableBase
+    {
+        private string message;
+
+        public string Message
+        {
+            get => this.message;
+            set => this.SetProperty(ref this.message, value);
+        }
+    }
+}
